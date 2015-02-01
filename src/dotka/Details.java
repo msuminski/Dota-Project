e43b4187d9e4885dbe7b822ba51a790b187745db
msuminski <mark.suminski@gmail.com>
2015-02-01 14:56:50 -0600

    Initial coding for the Dota Project

---
+++ src/dotka/Details.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dotka;
+
+import static dotka.Details.main;
+
+
+/**
+ *
+ * @author VRC Admin
+ */
+public class Details {
+
+
+public static void main(String [] args) {
+    
+    
+        
+          Dotka Earthshaker = new Dotka ("Earthshaker");
+    
+    }
+        
+   
+  
+
+
+public void getName() {
+    
+    System.out.println(Dotka.heroLevel(Details.Earthshaker));
+    }
+}
+
+
+
+
+
+    
---
+++ src/dotka/Dotka.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dotka;
+
+/**
+ *
+ * @author VRC Admin
+ */
+public class Dotka {
+    
+    static String heroName;
+    int heroLevel;
+    int heroStrength;
+    int heroAgility;
+    int heroIntelligence;
+    double strengthGain;
+    double agilityGain;
+    double intelligenceGain;
+
+    Dotka(String earthshaker) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+public void Dota(String name, int lvl, int str, int agi, int intel, double strgain, double agigain, double intelgain) {
+    
+    heroName = name;
+    heroLevel = lvl;
+    heroStrength = str;
+    heroAgility = agi;
+    heroIntelligence = intel;
+    strengthGain = strgain;
+    agilityGain = agigain;
+    intelligenceGain = intelgain;
+    
+}   
+
+
+}
+
+
+   
+
+
+    
+    
+    
+
+
+    
+    
+

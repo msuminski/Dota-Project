623526fcc0b7c9161088fe0e785e06a2b0bb65d9
msuminski <mark.suminski@gmail.com>
2015-02-21 13:04:45 -0600

    Initial coding for the Dota Project still

---
+++ src/heropedia/Earthshaker.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heropedia;
+import heropedia.Dotka;
+/**
+ *
+ * @author VRC Admin
+ */
+public class Earthshaker {
+    public static void main (String[] args){
+    
+    Dotka Earthshaker = new Dotka es("Earthshaker", 22, 12, 16, 2.9, 1.4, 1.8);
+    }
+}    
